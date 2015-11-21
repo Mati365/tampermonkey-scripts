7458e66949eb7cebd2d31e5b7326588e68818a5f
Mateusz Bagiński <cziken58@gmail.com>
2015-11-21 14:41:23 +0100

    Update cda-downloader.js

--- cda-downloader.js
+++ cda-downloader.js
@@ -2,17 +2,46 @@
 // @name         CDA Downloader
 // @namespace    http://github.com/Mati365
 // @version      0.1
-// @description  try to take over the world!
 // @author       Mateusz Bagiński <cziken58@gmail.com>
 // @match        http://www.cda.pl/video/*
 // @grant        none
 // ==/UserScript==
 'use strict';
 (function() {
-    window.onload = function() {
-        document.querySelector(".well-lite a").parentNode.innerHTML += 
-              "<a class='primaryButton' target='_blank' href='" 
-            + document.querySelector(".brdPlayerWrapper > .brdPlayer > div > video").src 
-            + "' download>Pobierz film</a>";
+    Element.prototype.setAttrs = function(attrs, html) {
+        for(var key in attrs)
+            this.setAttribute(key, attrs[key]);
+        if(void 0 !== html)
+            this.innerHTML = html;
+        return this;
     };
+    Element.prototype.setListener = function(event, callback) {
+        return this.addEventListener(event, callback) || this;
+    };
+    document
+        .querySelector(".well-lite div a[class*='primaryButton']")
+        .parentNode.appendChild(
+			document
+				.createElement("a")
+				.setListener("click", downloadMovie)
+				.setAttrs({
+						  class: "primaryButton"
+						, target: "_blank"
+				}, "Pobierz film")
+        );
+    function downloadMovie(e) {
+        var getStreamURL = function() {
+            var re = /type: 'html5'[\s\S]*file: '([^']*)'/gm
+              , match = null;
+            for(var script of [].slice.call(document.getElementsByTagName("script"))) {
+                if(match = re.exec(script.innerHTML))
+                    return match[1];
+            }
+        };
+        if(this.href === "")
+            this.setAttrs({
+                  href: getStreamURL()
+                , download: document.title.replace(/[\. ]/g, "_") + ".mp4"
+            });
+	};
 }());
